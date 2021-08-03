ea4c71e22d03bdc15a71aff21d6021e334d4e42d
Masha Kokhanchik <masha.kokhanchik@gmail.com>
2021-08-03 14:40:11 +0300

    Create project

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Мария Коханчик on 03.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
