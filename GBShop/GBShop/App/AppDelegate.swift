da2b65c9d38f558d43bf6424ddbcf238463018a7
mashakokhanchik <74599946+mashakokhanchik@users.noreply.github.com>
2021-10-15 11:40:09 +0300

    Created a product review screen with reviews (#13)

--- GBShop/GBShop/App/AppDelegate.swift
+++ GBShop/GBShop/App/AppDelegate.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import Swinject
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let requestFactory = RequestFactory()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
