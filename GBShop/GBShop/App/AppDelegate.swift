2397b858377cc77874de6551d7af55dc37467d7d
mashakokhanchik <74599946+mashakokhanchik@users.noreply.github.com>
2021-10-25 19:52:14 +0300

    Feature/hw11 (#17)
    * Created basket screen
    
    * Connecting the project to the Firebase

--- GBShop/GBShop/App/AppDelegate.swift
+++ GBShop/GBShop/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Swinject
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let requestFactory = RequestFactory()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
